ef38c4d85fa468bf5a63d47a1de0dd613e1ba1da
sok mean <mean77929@gmail.com>
2026-02-12 14:45:05 +0700

    o_0

--- c folder/codeYT/46_macro.c
+++ c folder/codeYT/46_macro.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-#define sq(x) x*x
+#define SQUARE(x) x*x
+
+#ifdef _WIN32
+    #define CLEAR_SCREEN "cls"
+#else
+    #define CLEAR_SCREEN "clear"
+#endif
+
+#define REPORT_ERROR(msg) printf("Error in %s at line %d: %s\n", __FILE__, __LINE__, msg);
+
+void clean() {
+    system(CLEAR_SCREEN);
+}
+
 int main(int argc, char *argv[]) {
-    printf("%d\n", sq(3));
+    clean();
+
+    printf("%d\n", SQUARE(3));
+
+    REPORT_ERROR("This is the test error");
 
     return 0;
 }
