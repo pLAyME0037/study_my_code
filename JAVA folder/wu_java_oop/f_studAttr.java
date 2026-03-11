3528062cf31e87f1bf2af34b679b5ed8c38d0e30
sok mean <mean77929@gmail.com>
2026-03-11 20:23:25 +0700

    java netwoking br pt 2 with rand java code

---
+++ JAVA folder/wu_java_oop/f_studAttr.java
@@ -0,0 +1,21 @@
+class f_studAttr {
+    public String name = null;
+    public int    age  = 0;
+
+    public static void main(String[] args) {
+        f_studAttr stud = new f_studAttr();
+        stud.display("Mean", 22);
+        stud.display("Jea", 20);
+        stud.display("bo", 40);
+        stud.display("neary", 10);
+        stud.display("Tola", 40);
+    }
+
+    public void display(String name, int age) {
+        this.name = name;
+        this.age  = age;
+
+        System.out.format("Student name %s. Student is %d years old. %n",
+                          name, age);
+    }
+}
