49c3ac7d745085515c4fc7897b9e8002870532de
sok mean <mean77929@gmail.com>
2026-03-19 20:23:56 +0700

    another flutter cloth comit

--- JAVA_folder/wu_java_oop/d_static.java
+++ JAVA_folder/wu_java_oop/d_static.java
@@ -1,16 +1,19 @@
 class d_static {
+    public static int counter = 0;
+    public static void counter() { counter++; }
+
+    static {
+        System.out.println("foo");
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello, World");
 
-        while (counter < 10) { counter(); }
+        while (counter < 12) { counter(); }
         System.out.println("number: " + counter);
 
         d_static.outer_class display_nested_method = new d_static.outer_class();
         display_nested_method.display();
-    }
-
-    static {
-        System.out.println("foo");
     }
 
     static class outer_class {
@@ -18,14 +21,12 @@ class d_static {
             System.out.println("from nested class");
         }
     }
-
-    public static int counter = 0;
-    public static void counter() { counter++; }
 }
 
-// class two {
-//     public static void main(String[] args) {
-//         d_static.counter();
-//         System.out.println("class tow: " + d_static.counter);
-//     }
-// }
+class two {
+    public static void main(String[] args) {
+        while (d_static.counter < 5) { d_static.counter(); }
+        System.out.println("------------------------------");
+        System.out.println("class two: " + d_static.counter);
+    }
+}
---
+++ flutter_folder/cloth_app/lib/search_screen/searchApp.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+//
+// class SearchScreen extends StatefulWidget {
+//     const SearchScreen ({super.key});
+//
+//     @override
+//     State<SearchScreen> createState() => _SearchScreenState();
+// }
+//
+// class _SearchScreenState extends State<SearchScreen> {
+//     @override
+//         Widget build(BuildContext context) {
+//             return Scaffold (
+//
+//             );
+//        }
+// }
+
+class SearchScreen extends StatelessWidget {
+  const SearchScreen ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+      return Scaffold ();
+  }
+}
+
