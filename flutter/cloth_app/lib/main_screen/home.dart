6bd5b1f4a4ed810a5d39243e361d0067c3348ef6
sok mean <mean77929@gmail.com>
2026-03-26 20:25:21 +0700

    flutter cloth app br pt #no_idk

--- flutter/cloth_app/lib/main_screen/home.dart
+++ flutter/cloth_app/lib/main_screen/home.dart
@@ -1,3 +1,4 @@
+import 'package:cloth_app/search_screen/searchApp.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -14,31 +15,37 @@ class _HomeScreenState extends State<HomeScreen> {
       length: 9,
       child: Scaffold (
         appBar: AppBar (
-          title: Container(
-            height: 35,
-            decoration: BoxDecoration(
-              border: Border.all(color: Colors.yellow, width: 1.4),
-              borderRadius: BorderRadius.circular(25)
-            ),
-            child: Row(children: [
-              Icon(
-                Icons.search,
-                color: Colors.grey),
-              Text(
-                'What are you looking for?',
-                 style: TextStyle(fontSize: 16, color: Colors.grey)),
-              Container(
-                height: 32,
-                width: 75,
+          title: InkWell (
+            onTap: () {
+                Navigator.push (
+                    context,
+                    MaterialPageRoute(builder: (context) => const SearchScreen())
+                );
+            },
+            child: Container(
+                height: 35,
                 decoration: BoxDecoration(
-                  color: Colors.yellow,
-                  borderRadius: BorderRadius.circular(10)
-                ),
-                child: Text(
-                  'search',
-                  style: TextStyle(color: Colors.grey)
-                ))
-            ])),
+                    border: Border.all(color: Colors.yellow, width: 1.4),
+                    borderRadius: BorderRadius.circular(15)),
+
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                    Icon(Icons.search, color: Colors.grey),
+                    Text(
+                        'What are you looking for?',
+                        style: TextStyle(fontSize: 16, color: Colors.grey)),
+                    Container(
+                        height: 32,
+                        width: 75,
+                        decoration: BoxDecoration(
+                            color: Colors.yellow,
+                            borderRadius: BorderRadius.circular(10)),
+
+                        child: Text(
+                            'search',
+                            style: TextStyle(color: Colors.grey)))
+            ]))),
           bottom: TabBar(tabs: [
             RepeatedTab(label: "Men"),
             RepeatedTab(label: "Woman"),
--- flutter/cloth_app/lib/search_screen/searchApp.dart
+++ flutter/cloth_app/lib/search_screen/searchApp.dart
@@ -1,5 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-//
+
 // class SearchScreen extends StatefulWidget {
 //     const SearchScreen ({super.key});
 //
@@ -17,11 +18,20 @@ import 'package:flutter/material.dart';
 // }
 
 class SearchScreen extends StatelessWidget {
-  const SearchScreen ({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-      return Scaffold ();
-  }
+    const SearchScreen ({super.key});
+    
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold (
+          appBar: AppBar(
+              elevation: 0,
+              backgroundColor: Colors.white,
+              leading: IconButton(
+                  icon: Icon(Icons.arrow_back_ios_new),
+                  onPressed: () {
+                      Navigator.pop(context);
+                  }),
+              title: CupertinoSearchTextField()));
+    }
 }
 
