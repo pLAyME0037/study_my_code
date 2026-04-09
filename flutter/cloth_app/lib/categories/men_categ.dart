a6adfb8ce4fdb15e6ca20e21c40d7a0385fcf84a
sok mean <mean77929@gmail.com>
2026-04-09 20:31:59 +0700

    flutter cloth app br pt 6

---
+++ flutter/cloth_app/lib/categories/men_categ.dart
@@ -0,0 +1,56 @@
+import 'package:cloth_app/widgets/CateWidgets.dart';
+import 'package:flutter/material.dart';
+
+class MenCategory extends StatelessWidget {
+  const MenCategory({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: SizedBox(
+              height: size.height * 0.8,
+              width: size.width * 0.75,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const CategHeaderLabel(
+                    headerLabel: 'Men',
+                  ),
+                  SizedBox(
+                    height: size.height * 0.68,
+                    child: GridView.count(
+                      mainAxisSpacing: 70,
+                      crossAxisCount: 3,
+                      children: List.generate(men.length, (index) {
+                        return SubcategModel(
+                          mainCategName: 'men',
+                          subCategName: men[index],
+                          assetName: 'images/men/men$index.jpg',
+                          assetLabel: men[index],
+                        );
+                      }),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+              bottom: 0,
+              right: 0,
+              child: SliderBar(
+                size: size,
+                sliderLabel: 'men',
+              ))
+        ],
+      ),
+    );
+  }
+}
