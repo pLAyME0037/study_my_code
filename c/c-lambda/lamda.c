cf36a7a639ed3fe898c43d68485a11c0334e9393
sok mean <mean77929@gmail.com>
2026-06-26 19:39:45 +0700

    bind_vars

--- c/c-lambda/lamda.c
+++ c/c-lambda/lamda.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -11,18 +12,37 @@ typedef enum {
     EXPR_VAR,
     EXPR_FUN,
     EXPR_APP,
-} Expr_kind;
+} Expr_Kind;
 
 typedef struct Expr Expr;
+
 typedef struct {
-    const char *arg;
+    const char *name;
+    size_t id;
+} Var_Name;
+
+Var_Name var_name_bound(const char *name, size_t id) {
+    Var_Name var = {
+        .name = name,
+        .id = id
+    };
+
+    return var;
+}
+
+Var_Name var_name_free(const char *name) {
+    var_name_bound(name, 0);
+}
+
+typedef struct {
+    Var_Name arg;
     Expr *body;
 } Expr_Fun;
 
 struct Expr {
-    Expr_kind kind;
+    Expr_Kind kind;
     union {
-        const char *var;
+        Var_Name var;
         Expr_Fun fun;
         struct {
             Expr *lhs;
@@ -35,11 +55,20 @@ Expr *var(const char *name) {
     Expr *expr = malloc(sizeof(*expr));
     assert(expr != NULL);
     expr->kind = EXPR_VAR;
-    expr->as.var = strdup(name);
+    expr->as.var = var_name_free(name);
     return expr;
 }
 
 Expr *fun(const char *arg, Expr *body) {
+    Expr *expr = malloc(sizeof(*expr));
+    assert(expr != NULL);
+    expr->kind = EXPR_FUN;
+    expr->as.fun.arg = var_name_free(arg);
+    expr->as.fun.body = body;
+    return expr;
+}
+
+Expr *fun_bound(Var_Name arg, Expr *body) {
     Expr *expr = malloc(sizeof(*expr));
     assert(expr != NULL);
     expr->kind = EXPR_FUN;
@@ -60,10 +89,13 @@ Expr *app(Expr *lhs, Expr *rhs) {
 void expr_display(Expr *expr, String_Builder *builder) {
     switch (expr->kind) {
     case EXPR_VAR:
-        sb_appendf(builder, "%s", expr->as.var);
+        sb_appendf(builder, "%s", expr->as.var.name);
+        if (expr->as.var.id > 0) {
+            sb_appendf(builder, "%zu", expr->as.var.id);
+        }
     break;
     case EXPR_FUN:
-        sb_appendf(builder, "(λ%s.", expr->as.fun.arg);
+        sb_appendf(builder, "(λ%s%zu.", expr->as.fun.arg.name, expr->as.fun.arg.id);
         expr_display(expr->as.fun.body, builder);
         sb_appendf(builder, ")");
     break;
@@ -74,22 +106,24 @@ void expr_display(Expr *expr, String_Builder *builder) {
         expr_display(expr->as.app.rhs, builder);
         sb_appendf(builder, ")");
     break;
-    default: UNREACHABLE("Expr_kind");
+    default: UNREACHABLE("Expr_Kind");
     }
 }
 
-Expr *replace(const char *arg, Expr *body, Expr *val) {
+Expr *replace(Var_Name arg, Expr *body, Expr *val) {
     switch (body->kind) {
     case EXPR_VAR:
-        if (strcmp(body->as.var, arg) == 0) { return val; }
+        if (strcmp(body->as.var.name, arg.name) == 0 && body->as.var.id == arg.id) {
+            return val;
+        }
         return body;
     case EXPR_FUN:
-        return fun(body->as.fun.arg, replace(arg, body->as.fun.body, val));
+        return fun_bound(body->as.fun.arg, replace(arg, body->as.fun.body, val));
     case EXPR_APP:
         return app(
             replace(arg, body->as.app.lhs, val),
             replace(arg, body->as.app.rhs, val));
-    default: UNREACHABLE("Expr_kind");
+    default: UNREACHABLE("Expr_Kind");
     }
 }
 
@@ -99,8 +133,13 @@ Expr *apply(Expr_Fun fun, Expr *val) {
 
 Expr *eval1(Expr *expr) {
     switch (expr->kind) {
-    case EXPR_VAR:
-    case EXPR_FUN: return expr;
+    case EXPR_VAR: return expr;
+    case EXPR_FUN: 
+        Expr *body = eval1(expr->as.fun.body);
+        if (body != expr->as.fun.body) {
+            return fun_bound(expr->as.fun.arg, body);
+        }
+        return expr;
     case EXPR_APP:
         Expr *lhs = eval1(expr->as.app.lhs);
 
@@ -118,7 +157,7 @@ Expr *eval1(Expr *expr) {
         } 
 
         return expr;
-    default: UNREACHABLE("Expr_kind");
+    default: UNREACHABLE("Expr_Kind");
     }
 
 }
@@ -130,6 +169,45 @@ void trace_expr(Expr *expr, String_Builder *str_b) {
     printf("%s\n", str_b->items);
 }
 
+
+void bind_var(Expr *body, Var_Name var) {
+    switch (body->kind) {
+    case EXPR_VAR: {
+        if (strcmp(body->as.var.name, var.name) == 0) {
+            body->as.var.id = var.id;
+        }
+    } break;
+    case EXPR_FUN: {
+        bind_var(body->as.fun.body, var);
+    } break;
+    case EXPR_APP: {
+        bind_var(body->as.app.lhs, var);
+        bind_var(body->as.app.rhs, var);
+    } break;
+    default: UNREACHABLE("Expr_Kind");
+    }
+}
+
+Expr *bind_vars(Expr *expr) {
+    switch (expr->kind) {
+    case EXPR_VAR: return expr;
+    case EXPR_FUN: {
+        static size_t id_counter = 1;
+
+        assert(expr->as.fun.arg.id == 0);
+        expr->as.fun.arg.id = id_counter++;
+        bind_var(expr->as.fun.body, expr->as.fun.arg);
+        bind_vars(expr->as.fun.body);
+    } break;
+    case EXPR_APP: {
+        bind_vars(expr->as.app.lhs);
+        bind_vars(expr->as.app.rhs);
+        return expr;
+    }
+    default: UNREACHABLE("Expr_Kind");
+    }
+}
+
 int main(void) {
 
     String_Builder str_b = {0};
@@ -138,7 +216,10 @@ int main(void) {
     // Expr *expr = app(
     //         fun("x", app(var("x"), var("x"))),
     //         fun("x", app(var("x"), var("x"))));
-    Expr *expr = app(app(fun("y",fun("x", var("y"))), var("x")), var("else"));
+    // Expr *expr = bind_vars(
+    //     app(app(fun("y",fun("x", var("y"))), var("x")), var("pp"))
+    // );
+    Expr *expr = bind_vars(app(fun("y", app(fun("x", var("x")), var("y"))), var("pp")));
 
     trace_expr(expr, &str_b);
 
