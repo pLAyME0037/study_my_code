f0ccb10bc9fbc253a31b7f71e083346e88020185
sok mean <mean77929@gmail.com>
2026-07-29 17:55:22 +0700

    now with gtk

--- c/data-tokenizer/main.c
+++ c/data-tokenizer/main.c
@@ -1,18 +1,39 @@
+#include <gtk/gtk.h>
+#include "ui/window.h"
 #include "third_party/hash_table.h"
 #include "third_party/json_format.h"
 
-int main(void) {
-    SensitiveReport report = {0};
+int main(int argc, char **argv) {
+    // const char *file_path = "./data_2mb.json";
 
-    if (!find_sensetive_data("./data_2mb.json", &report)) return 1;
+    // {
+    //     String_Builder sb = {0};
+    //     if (!nob_read_entire_file(file_path, &sb)) return 1;
+    //     String_View content = {
+    //         .count = sb.count,
+    //         .data  = sb.items,
+    //     };
+    //
+    //     if (hash_analysis(content, file_path)) return 1;
+    // }
+    // {
+    //     SensitiveReport report = {0};
+    //
+    //     if (!find_sensetive_data(file_path, &report)) return 1;
+    //
+    //     confirm_sensitive_fields(&report);
+    //     print_report(&report);
+    //
+    //     free(report.items);
+    // }
 
-    confirm_sensitive_fields(&report);
-    print_report(&report);
+    GtkApplication *app = gtk_application_new("token.app", G_APPLICATION_DEFAULT_FLAGS);
 
-    // TODO: pass &report to hash_analysis for indexing
-    // String_View content = ...;
-    // hash_analysis(content, &report);
+    g_signal_connect(app, "activate", G_CALLBACK(app_window_new), NULL);
 
-    free(report.items);
-    return 0;
+    int status = g_application_run(G_APPLICATION(app), argc, argv);
+
+    g_object_unref(app);
+
+    return status;
 }
---
+++ c/data-tokenizer/ui/window.h
@@ -0,0 +1,7 @@
+#ifndef WINDOW
+#define WINDOW
+
+#include <gtk/gtk.h>
+
+GtkWidget *app_window_new(GtkApplication *app);
+#endif // !WINDOW
