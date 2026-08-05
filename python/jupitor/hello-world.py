a18c5f931acfb5ee63358c49953460d4bc784093
sok mean <mean77929@gmail.com>
2026-08-05 18:25:33 +0700

    plan to learn webc

--- python/jupitor/hello-world.py
+++ python/jupitor/hello-world.py
@@ -1,3 +1,15 @@
 #!./path/to/venv/bin/python3
 
+import numpy as np
+
 print("Hello, World")
+
+arr1 = np.array([1, 2, 3, 4])
+arr2 = np.array([[1, 2], [3, 4]])
+arr3 = np.array([[1, 2, 4], [3, 4, 5]])
+
+print(arr1.ndim)
+print(arr2.ndim)
+print(arr3.shape)
+print(arr1.dtype)
+print(arr1[3:0])
